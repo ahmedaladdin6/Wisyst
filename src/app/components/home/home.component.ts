e5b7e1b7a89183215d69514a020c1808b99a6170
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2020-08-01 05:10:57 +0200

    make home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  activites=[];
+  partners=[];
+  constructor() { 
+    this.activites =[
+      { image:'../../../assets/images/1.jpg',alt:'1',button:'مهرجان كان يا مكان لاحياء التراث الوطني بحضور رئيس اكادمية الشعر العربي'},
+      { image:'../../../assets/images/3.jpg',alt:'3',button:'حملة معا ضد الصرع'},
+      { image:'../../../assets/images/2.jpg',alt:'1',button:'يعلن معهد اليقظة عن بدء التسجيل في دوره اسرار وفنون المكياج للميك اب ارتيست'},
+    ];
+
+    this.partners=[
+      {image:'../../../assets/images/partner1.jpg',alt:'partner1'},
+      {image:'../../../assets/images/partner2.jpeg',alt:'partner2'},
+      {image:'../../../assets/images/partner3.jpeg',alt:'partner3'},
+    ]
+
+  }
 
   ngOnInit(): void {
   }
